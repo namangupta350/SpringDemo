7d24b947cee395bada659837a9ef416eea53d410
Tanya Gupta <Tanya Gupta@192.168.1.11>
2022-07-04 13:33:41 +0530

    adding newly saved customer.java from entity package

--- web-customer-tracker/src/com/luv2code/springdemo/entity/Customer.java
+++ web-customer-tracker/src/com/luv2code/springdemo/entity/Customer.java
@@ -25,4 +25,47 @@ public class Customer {
 	@Column(name="email")
 	private String email;
 	
+	public Customer() {
+		
+	}
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [Id=" + Id + ", firstname=" + firstname + ", lastname=" + lastname + ", email=" + email + "]";
+	}
+	
+	
+	
 }
