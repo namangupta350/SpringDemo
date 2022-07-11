a65bd8d8bae0a585140b371ca626dca397021cc7
Tanya Gupta <Tanya Gupta@192.168.1.11>
2022-07-11 18:06:18 +0530

    adding AOP Project

---
+++ spring-demo-aop/src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,16 @@
+package com.luv2code.aopdemo.dao;
+
+import java.util.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDAO {
+	
+	public void addAccount() {
+		
+	System.out.println(getClass()+"Doing my db work");
+	
+	
+	
+	}
+}
