0177216d1e5088db0ffda797e0d4b824e70b8ba7
Tanya Gupta <Tanya Gupta@192.168.1.11>
2022-07-12 12:26:29 +0530

    updated membership demo

---
+++ spring-demo-aop/src/com/luv2code/aopdemo/dao/MembershipDAO.java
@@ -0,0 +1,18 @@
+package com.luv2code.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MembershipDAO {
+	
+	public boolean NewAccount() {
+		
+		System.out.println(" In membership class ");
+		
+		System.out.println("In addAccount");
+		
+		return true;
+		
+	}
+
+}
