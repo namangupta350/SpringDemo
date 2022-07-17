c420c18faafb5f315bddfe0afc59f5309895772c
Tanya Gupta <Tanya Gupta@192.168.1.11>
2022-07-17 19:01:01 +0530

    adding new maven project

--- spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,13 +2,17 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 	
-	@Before("execution(public boolean add*())")
+	@Pointcut("execution(public boolean add*())")
+	public void forPointcutreuse() {}
+	
+	@Before("forPointcutreuse()")
 	public void beforeAddAccountAdvise() {
 		
 		System.out.println("=======> Executing @before advice ");
